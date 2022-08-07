cef326e97e316556b3735546c80cac6331ae6504
Jarne Staelens <jarne.s107@gmail.com>
2022-08-07 11:20:06 +0200

    1.7 Rechts draaien

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 function AllesAfwerken () {
+    Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+    Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
     basic.showIcon(IconNames.Happy)
 }
 function Aftellen (vanaf: number) {
@@ -9,6 +11,15 @@ function Aftellen (vanaf: number) {
 input.onButtonPressed(Button.A, function () {
     Aftellen(3)
     DoeIets(10)
+    AllesAfwerken()
+})
+function StartLinksVooruit () {
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 26)
+}
+input.onButtonPressed(Button.B, function () {
+    Aftellen(3)
+    StartLinksVooruit()
+    basic.pause(1000)
     AllesAfwerken()
 })
 function DoeIets (seconden: number) {
