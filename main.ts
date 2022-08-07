975c909d75546bd54ce662caf64d0a28f3bcf7fe
Jarne Staelens <jarne.s107@gmail.com>
2022-08-07 10:06:15 +0200

    1.3 Aantonen dat de robot klaar is voor een nieuwe opdracht

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+function AllesAfwerken () {
+    basic.showIcon(IconNames.Happy)
+}
+input.onButtonPressed(Button.A, function () {
+    DoeIets()
+    AllesAfwerken()
+})
+function DoeIets () {
+    for (let index = 0; index < 4; index++) {
+        basic.showString("/")
+        basic.pause(100)
+        basic.showString("\\")
+        basic.pause(100)
+    }
+}
+AllesAfwerken()
