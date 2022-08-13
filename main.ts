4d3b7c8b4b25755ae205e6349b5a0778e62483e3
Jarne Staelens <jarne.s107@gmail.com>
2022-08-13 11:24:49 +0200

    1.12 Traag en rechtdoor rijden, 1.13 Het spoor detecteren en 1.14 Het spoor volgen

--- main.ts
+++ main.ts
@@ -15,6 +15,16 @@ function BeweegVooruit (seconden: number) {
         basic.showString("\\")
     }
 }
+function VolgSpoorRechts () {
+    Info("Rechter sensor loopt boven het spoor?")
+    if (Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right) == 1 || Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right) == 0) {
+        rechterSensorZietWit = false
+        StartLinksVooruit()
+    } else {
+        rechterSensorZietWit = true
+        Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
+    }
+}
 function StartRechtsVooruit () {
     Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 26)
 }
@@ -31,19 +41,39 @@ input.onButtonPressed(Button.A, function () {
 function StartLinksVooruit () {
     Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 26)
 }
+function VolgSpoor (seconden: number) {
+    maximumLoopTijd = input.runningTime() / (seconden * 1000)
+    blijfRijden = true
+    while (blijfRijden) {
+        Info("Motoren Aansturen")
+        VolgSpoorLinks()
+        VolgSpoorRechts()
+        Info("Stop waneer de tijd op is")
+        if (input.runningTime() >= maximumLoopTijd) {
+            blijfRijden = false
+        }
+        Info("Stop waneer het spoor onzichtbaar is")
+        if (linkerSensorZietWit && rechterSensorZietWit) {
+            blijfRijden = false
+        }
+    }
+    Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+    Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
+}
 input.onButtonPressed(Button.B, function () {
     Aftellen(3)
-    Info("Rechts starten om de motor links te draaien.")
-    StartRechtsVooruit()
-    basic.pause(1000)
-    Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
-    // Links starten door de motor rechts te draaien.
-    StartLinksVooruit()
-    basic.pause(1000)
-    Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
-    // Afwerken.
-    AllesAfwerken()
+    VolgSpoor(10)
 })
+function VolgSpoorLinks () {
+    Info("Linker sensor loopt boven het spoor?")
+    if (Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left) == 1 || Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left) == 0) {
+        linkerSensorZietWit = false
+        StartRechtsVooruit()
+    } else {
+        linkerSensorZietWit = true
+        Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+    }
+}
 function DoeIets (seconden: number) {
     maximumLoopTijd = input.runningTime() + seconden * 1000
     while (input.runningTime() < maximumLoopTijd) {
@@ -53,5 +83,8 @@ function DoeIets (seconden: number) {
         basic.pause(100)
     }
 }
+let linkerSensorZietWit = false
+let blijfRijden = false
+let rechterSensorZietWit = false
 let maximumLoopTijd = 0
 AllesAfwerken()
