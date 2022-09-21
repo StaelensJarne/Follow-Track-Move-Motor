856526712de1aab3c62cdc752fcea4b486051501
Jarne Staelens <jarne.s107@gmail.com>
2022-09-21 20:22:12 +0200

    Algemene update

--- main.ts
+++ main.ts
@@ -1,57 +1,49 @@
 // Tel van 4 tot 0 en verander telkens 1 LED van kleur.
 function Aftellen () {
     for (let index = 0; index <= 4; index++) {
+        basic.showNumber(4 - index)
         // Kitronik_Move_Motor.ZipLedColors.GREEN = 65280
         moveMotorZIP.setZipLedColor(index, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Green))
         moveMotorZIP.show()
-        basic.pause(1000)
+        basic.pause(500)
     }
 }
 // Waneer op knop A gedrukt word tel af en volg het spoor geduurende 10 seconden.
 input.onButtonPressed(Button.A, function () {
     Aftellen()
-    VolgSpoor(10)
-    ZetMotorKlaar()
+    VolgSpoor(5)
+    ZetMoveMotorKlaar()
 })
-// Zet de :Move motor klaar voor hij de lijn zal volgen.
-function ZetMotorKlaar () {
-    moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
-    // Kitronik_Move_Motor.ZipLedColors.BLUE = 255
-    ZetMotorLichten(255)
-    Kitronik_Move_Motor.stop()
-}
-// Bereken het verschil tussen de linkse en rechtse sensor.
-function BerekenSensorVerschil () {
-    sensorRechts = LeesLijnVolgSensorRechts()
-    sensorLinks = LeesLijnVolgSensorLinks()
-    sensorVerschil = Math.abs(sensorRechts - sensorLinks)
-    return sensorVerschil
-}
 // Lees de waarde van de rechter lijnvolgsensor. Deze kan tussen de 0 en de 1023 liggen.
 function LeesLijnVolgSensorRechts () {
     return Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
 }
 // De functie om het spoor te volgen.
 function VolgSpoor (seconden: number) {
-    let waardeVolgSpoor: number;
-VolgSpoorTijd = input.runningTime() + seconden * 1000
-    while (input.runningTime() < VolgSpoorTijd) {
-        basic.showNumber(waardeVolgSpoor)
-        waardeVolgSpoor = BerekenSensorVerschil()
-        if (18 < waardeVolgSpoor && waardeVolgSpoor < 25) {
-            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 5)
-        } else if (waardeVolgSpoor < 25) {
-            Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 5)
-        } else if (waardeVolgSpoor > 35) {
-            Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, 5)
+    let maximumLoopTijd = 0
+    while (maximumLoopTijd >= seconden) {
+        sensorRechts = LeesLijnVolgSensorRechts()
+        sensorLinks = LeesLijnVolgSensorLinks()
+        sensorVerschil = Math.abs(sensorLinks - sensorRechts)
+        if (sensorVerschil > 10) {
+            if (sensorLinks > sensorRechts) {
+                Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
+                Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 1)
+            } else {
+                Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+                Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 1)
+            }
+        } else {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 1)
         }
     }
-    Kitronik_Move_Motor.stop()
 }
-// Waneer op knop B gedrukt wordt stop de :Move motor.
-input.onButtonPressed(Button.B, function () {
-    Kitronik_Move_Motor.stop()
-})
+// Zet de :Move motor klaar voor hij de lijn zal volgen.
+function ZetMoveMotorKlaar () {
+    moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
+    // Kitronik_Move_Motor.ZipLedColors.BLUE = 255
+    ZetMotorLichten(255)
+}
 // Zet de LEDs op een andere kleur. De waarden kan je terugvinden op: https://github.com/KitronikLtd/pxt-kitronik-zip-64/blob/master/zip64.ts.
 function ZetMotorLichten (kleur: number) {
     moveMotorZIP.setColor(Kitronik_Move_Motor.colors(kleur))
@@ -61,9 +53,8 @@ function ZetMotorLichten (kleur: number) {
 function LeesLijnVolgSensorLinks () {
     return Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
 }
-let VolgSpoorTijd = 0
 let sensorVerschil = 0
 let sensorLinks = 0
 let sensorRechts = 0
 let moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = null
-ZetMotorKlaar()
+ZetMoveMotorKlaar()
