4768aefaa4d7d3c9da00c8f09ebba2903f72a39d
Jarne Staelens <jarne.s107@gmail.com>
2022-09-22 20:25:15 +0200

    Oefening afgewerkt

--- main.py
+++ main.py
@@ -1,66 +1,50 @@
-# Tel van 4 tot 0 en verander telkens 1 LED van kleur.
 def Aftellen():
     for index in range(5):
-        # Kitronik_Move_Motor.ZipLedColors.GREEN = 65280
+        basic.show_number(4 - index)
         moveMotorZIP.set_zip_led_color(index,
             Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.GREEN))
         moveMotorZIP.show()
         basic.pause(1000)
-# Waneer op knop A gedrukt word tel af en volg het spoor geduurende 10 seconden.
 
 def on_button_pressed_a():
     Aftellen()
-    VolgSpoor(10)
-    ZetMotorKlaar()
+    VolgSpoor(25)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-# Zet de :Move motor klaar voor hij de lijn zal volgen.
-def ZetMotorKlaar():
-    global moveMotorZIP
-    moveMotorZIP = Kitronik_Move_Motor.create_move_motor_zipled(4)
-    # Kitronik_Move_Motor.ZipLedColors.BLUE = 255
-    ZetMotorLichten(255)
-    Kitronik_Move_Motor.stop()
-# Bereken het verschil tussen de linkse en rechtse sensor.
-def BerekenSensorVerschil():
-    global sensorRechts, sensorLinks, sensorVerschil
-    sensorRechts = LeesLijnVolgSensorRechts()
-    sensorLinks = LeesLijnVolgSensorLinks()
-    sensorVerschil = abs(sensorRechts - sensorLinks)
-    return sensorVerschil
-# Lees de waarde van de rechter lijnvolgsensor. Deze kan tussen de 0 en de 1023 liggen.
 def LeesLijnVolgSensorRechts():
     return Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.RIGHT)
-# De functie om het spoor te volgen.
 def VolgSpoor(seconden: number):
-    global VolgSpoorTijd
-    VolgSpoorTijd = input.running_time() + seconden * 1000
-    while input.running_time() < VolgSpoorTijd:
-        basic.show_number(waardeVolgSpoor)
-        waardeVolgSpoor = BerekenSensorVerschil()
-        if 18 < waardeVolgSpoor and waardeVolgSpoor < 25:
-            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 5)
-        elif waardeVolgSpoor < 25:
-            Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.LEFT, 5)
-        elif waardeVolgSpoor > 35:
-            Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.RIGHT, 5)
-    Kitronik_Move_Motor.stop()
-# Waneer op knop B gedrukt wordt stop de :Move motor.
-
-def on_button_pressed_b():
-    Kitronik_Move_Motor.stop()
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-# Zet de LEDs op een andere kleur. De waarden kan je terugvinden op: https://github.com/KitronikLtd/pxt-kitronik-zip-64/blob/master/zip64.ts.
-def ZetMotorLichten(kleur: number):
+    global maximumLoopTijd, sensorRechts, sensorLinks, sensorVerschil
+    maximumLoopTijd = input.running_time() + seconden * 1000
+    while input.running_time() < maximumLoopTijd:
+        sensorRechts = LeesLijnVolgSensorRechts()
+        sensorLinks = LeesLijnVolgSensorLinks()
+        sensorVerschil = abs(sensorLinks - sensorRechts)
+        if sensorVerschil > 10:
+            if sensorLinks > sensorRechts:
+                Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_RIGHT)
+                Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
+                    Kitronik_Move_Motor.MotorDirection.FORWARD,
+                    1)
+            else:
+                Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_LEFT)
+                Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
+                    Kitronik_Move_Motor.MotorDirection.FORWARD,
+                    1)
+        else:
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 1)
+def ZetMoveMotorKlaar():
+    global moveMotorZIP
+    moveMotorZIP = Kitronik_Move_Motor.create_move_motor_zipled(4)
+    ZetLEDLichten(255)
+def ZetLEDLichten(kleur: number):
     moveMotorZIP.set_color(Kitronik_Move_Motor.colors(kleur))
     moveMotorZIP.show()
-# Lees de waarde van de linker lijnvolgsensor. Deze kan tussen de 0 en de 1023 liggen.
 def LeesLijnVolgSensorLinks():
     return Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.LEFT)
-VolgSpoorTijd = 0
 sensorVerschil = 0
 sensorLinks = 0
 sensorRechts = 0
+maximumLoopTijd = 0
 moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = None
-ZetMotorKlaar()
+ZetMoveMotorKlaar()
--- main.ts
+++ main.ts
@@ -1,60 +1,56 @@
-// Tel van 4 tot 0 en verander telkens 1 LED van kleur.
 function Aftellen () {
     for (let index = 0; index <= 4; index++) {
         basic.showNumber(4 - index)
-        // Kitronik_Move_Motor.ZipLedColors.GREEN = 65280
         moveMotorZIP.setZipLedColor(index, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Green))
         moveMotorZIP.show()
-        basic.pause(500)
+        basic.pause(1000)
     }
 }
-// Waneer op knop A gedrukt word tel af en volg het spoor geduurende 10 seconden.
 input.onButtonPressed(Button.A, function () {
     Aftellen()
-    VolgSpoor(5)
+    seconden = 60
+    maximumLoopTijd = input.runningTime() + seconden * 1000
+    ZetLEDLichten(16711935)
+    while (input.runningTime() < maximumLoopTijd) {
+        basic.pause(5)
+        VolgSpoor()
+    }
     ZetMoveMotorKlaar()
 })
-// Lees de waarde van de rechter lijnvolgsensor. Deze kan tussen de 0 en de 1023 liggen.
 function LeesLijnVolgSensorRechts () {
     return Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
 }
-// De functie om het spoor te volgen.
-function VolgSpoor (seconden: number) {
-    let maximumLoopTijd = 0
-    while (maximumLoopTijd >= seconden) {
-        sensorRechts = LeesLijnVolgSensorRechts()
-        sensorLinks = LeesLijnVolgSensorLinks()
-        sensorVerschil = Math.abs(sensorLinks - sensorRechts)
-        if (sensorVerschil > 10) {
-            if (sensorLinks > sensorRechts) {
-                Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
-                Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 1)
-            } else {
-                Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
-                Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 1)
-            }
+function VolgSpoor () {
+    sensorRechts = LeesLijnVolgSensorRechts()
+    sensorLinks = LeesLijnVolgSensorLinks()
+    sensorVerschil = Math.abs(sensorLinks - sensorRechts)
+    if (sensorVerschil > 10) {
+        if (sensorLinks > sensorRechts) {
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 1)
         } else {
-            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 1)
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 1)
         }
+    } else {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 1)
     }
 }
-// Zet de :Move motor klaar voor hij de lijn zal volgen.
 function ZetMoveMotorKlaar () {
     moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
-    // Kitronik_Move_Motor.ZipLedColors.BLUE = 255
-    ZetMotorLichten(255)
+    ZetLEDLichten(255)
 }
-// Zet de LEDs op een andere kleur. De waarden kan je terugvinden op: https://github.com/KitronikLtd/pxt-kitronik-zip-64/blob/master/zip64.ts.
-function ZetMotorLichten (kleur: number) {
+function ZetLEDLichten (kleur: number) {
     moveMotorZIP.setColor(Kitronik_Move_Motor.colors(kleur))
     moveMotorZIP.show()
 }
-// Lees de waarde van de linker lijnvolgsensor. Deze kan tussen de 0 en de 1023 liggen.
 function LeesLijnVolgSensorLinks () {
     return Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
 }
 let sensorVerschil = 0
 let sensorLinks = 0
 let sensorRechts = 0
+let maximumLoopTijd = 0
+let seconden = 0
 let moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = null
 ZetMoveMotorKlaar()
